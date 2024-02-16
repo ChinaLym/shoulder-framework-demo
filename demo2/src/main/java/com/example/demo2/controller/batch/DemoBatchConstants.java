31ef156b796ca57fa857ca331a288b585928b7e2
lym <lymdhr@qq.com>
2024-02-16 15:51:07 +0800

    remove dump class

--- demo2/src/main/java/com/example/demo2/controller/batch/DemoBatchConstants.java
+++ demo2/src/main/java/com/example/demo2/controller/batch/DemoBatchConstants.java
@@ -5,11 +5,6 @@ package com.example.demo2.controller.batch;
  */
 public interface DemoBatchConstants {
 
-    String DATA_TYPE_PERSON = "person";
-
-
-    String OPERATION_VALIDATE = "validate";
-
 
     String DATA_TYPE_TEST = "testBatchDataType";
 }
